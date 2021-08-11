0760ad5f7335cd0368deb8379dfef8d92b11f5eb
Hasan <hasanaburayyan21@gmail.com>
2021-08-10 22:32:52 -0400

    added comments

--- src/models/IVisitor.ts
+++ src/models/IVisitor.ts
@@ -1,4 +1,8 @@
 import { Node } from './parse-tree';
+
+/**
+ * Visitor interface
+ */
 export interface IVisitor {
   preVisit(node: Node): void;
   visit(node: Node): void;
--- src/tree-parser.ts
+++ src/tree-parser.ts
@@ -2,6 +2,21 @@
 import * as cdk from '@aws-cdk/core';
 import { ParseTree } from './models/parse-tree';
 
+/**
+ * Construct Tree Parser which when given an cdk app, it will generate different tree structures and
+ * meta data
+ *
+ * @example
+ *
+ * // Create A New ConstructTreeParser
+ * // First create a cdk app object
+ * const app = new cdk.App()
+ * // Add Constructs/Stacks to your app
+ * new SomeStack(app, 'some-stack-id', props)
+ * // Create ConstructTreeParser
+ * new ConstructTreeParser(app)
+ *
+ */
 export class ConstructTreeParser {
   private readonly _rootNode: cdk.App;
 
@@ -9,14 +24,35 @@ export class ConstructTreeParser {
     this._rootNode = node;
   }
 
+  /**
+   * Generate and return Parse Tree which implements visitor pattern methods (preVisit, visit, postVisit)
+   *
+   * You may not need the Parse Tree unless you intend to do further processing, rather you can just generate
+   * structures using existing generation functions.
+   */
   public generateParseTree(): ParseTree {
     return new ParseTree(this._rootNode);
   }
 
+  /**
+   * Generate a tree structure string
+   *
+   * @example
+   * new ConstructTreeParser(app).generateTreeStructure();
+   *
+   * Tree
+   * some-stack
+   *  `-- some-bucket-id
+   *      |-- Notifications
+   *      `-- Resource
+   */
   public generateTreeStructure(): string {
     return new ParseTree(this._rootNode).genTreeStructure();
   }
 
+  /**
+   * Return back the root node of the Parse Tree which is a cdk app
+   */
   public get rootNode(): cdk.App {
     return this._rootNode;
   }
